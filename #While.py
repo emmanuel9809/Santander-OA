96de30e39d1e766cb86afc0afa163a430e2b996f
emmanuel9809 <143969131+emmanuel9809@users.noreply.github.com>
2024-10-01 12:11:11 -0600

    Add files via upload

---
+++ #While.py
@@ -0,0 +1,8 @@
+#While
+
+contador=0
+
+while contador <5:
+    print(contador)
+    contador +=1
+
---
+++ bucles y loops.py
@@ -0,0 +1,6 @@
+# Bucles y loops.
+# for es iterable y se ocupa en tuplas, cadenas, listas
+
+frutas=["naranja","manzana","guayaba","sandia","pera","uva","coco","fresa","cereza","frambuesa","limon"]
+for fruta in frutas:
+    print(fruta)
