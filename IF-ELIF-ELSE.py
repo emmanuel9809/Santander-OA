8b17faa8b63a1c9cf6f51aa1b722b839f33948b7
emmanuel9809 <143969131+emmanuel9809@users.noreply.github.com>
2024-10-01 11:14:34 -0600

    Add files via upload

---
+++ IF-ELIF-ELSE.py
@@ -0,0 +1,8 @@
+calificacion = 85  # Asignación de la calificación
+
+if calificacion >= 85:
+    print("Materia aprobada")
+elif calificacion < 85:
+    print("Presentará primer vuelta de examen final")
+else:
+    print("No aprobatoria sin derecho a examen final")
---
+++ estructuras condicionales 2.py
@@ -0,0 +1,11 @@
+# La estructura IF-ELSE nos permite que si la condicion if es falsa ejecute un else
+
+edad=15
+
+if edad >=18:
+   print ("Eres mayor de edad")
+
+else:
+   print("Eres menor de edad")
+
+# En este caso si la edad es mayor o igual que 18 se ejecutara el bloque de codigo de if y si se determina lo contrario se ejecutara un else imprimiendo ("Eres menor de edad")
